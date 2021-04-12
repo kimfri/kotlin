14262b7f7d76b178fbc2f5aedc033a20519151b1
Kim Fritzon-Ölander <kim.fritzon@gmail.com>
2021-04-12 19:13:44 +0200

    Add test: implement by XX

---
+++ src/main/kotlin/interfaces/Animals.kt
@@ -0,0 +1,22 @@
+package interfaces
+
+interface CanEat {
+    fun eat(food: String): Boolean
+}
+
+interface CanRun {
+    fun run(): String
+}
+
+class FastMover: CanRun {
+    override fun run(): String {
+        return "Fast"
+    }
+
+}
+
+class Dog: CanEat, CanRun by FastMover() {
+    override fun eat(food: String): Boolean {
+        TODO("Not yet implemented")
+    }
+}
