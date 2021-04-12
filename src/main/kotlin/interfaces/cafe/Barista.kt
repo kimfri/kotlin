068619f09545ea7c3793c54b18de5a6cd328bca6
Kim Fritzon-Ölander <kim.fritzon@gmail.com>
2021-04-12 21:01:32 +0200

    Use anonomous callback
    Instead of implementing the callback interface
    on class-level and override fun do it in a
    anonomous way instead. This will save some lines
    of code, and is a bit cleaner to read.

--- src/main/kotlin/interfaces/cafe/Barista.kt
+++ src/main/kotlin/interfaces/cafe/Barista.kt
@@ -1,14 +1,13 @@
 package interfaces.cafe
 
-class Barista(private val name: String) : OnCoffeeBrewedListener {
+class Barista(private val name: String) {
     private val coffeeMaker = CoffeeMaker()
 
     fun acceptOrder(type: CoffeeType) {
-        coffeeMaker.brewCoffee(type, this)
-
-    }
-
-    override fun onCoffeeBrewed(coffee: Coffee) {
-        println("$name: finished brewing ${coffee.type}")
+        coffeeMaker.brewCoffee(type, object : OnCoffeeBrewedListener {
+            override fun onCoffeeBrewed(coffee: Coffee) {
+                println("$name: finished brewing ${coffee.type}")
+            }
+        })
     }
 }
